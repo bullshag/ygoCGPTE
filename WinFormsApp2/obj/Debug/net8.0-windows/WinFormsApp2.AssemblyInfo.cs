7ced017b3bbd7b20403823d1674205c5ae2c3d4e
bullshag <78245141+bullshag@users.noreply.github.com>
2025-08-09 21:33:47 -0500

    r

--- WinFormsApp2/obj/Debug/net8.0-windows/WinFormsApp2.AssemblyInfo.cs
+++ WinFormsApp2/obj/Debug/net8.0-windows/WinFormsApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c10852a2a991b0f57626e94777a61f1cff3b05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc857454bc20ecad7ebd8b8e2a87b611ba009ac3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
