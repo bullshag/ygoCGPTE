ef9d159b2724cf20da380a62b9d72fb89d272762
bullshag <78245141+bullshag@users.noreply.github.com>
2025-08-26 14:03:55 -0500

    d

--- WinFormsApp2/NavigationWindow.Designer.cs
+++ WinFormsApp2/NavigationWindow.Designer.cs
@@ -28,12 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "NavigationWindow";
+            pictureBox1 = new PictureBox();
+            nodeRiverVillage = new RadioButton();
+            nodeMounttown = new RadioButton();
+            nodeDesert = new RadioButton();
+            nodeDarkSpire = new RadioButton();
+            nodeSouthernIsland = new RadioButton();
+            nodeMountain = new RadioButton();
+            nodeForestPlains = new RadioButton();
+            nodeNorthernIsland = new RadioButton();
+            nodeFarCliffs = new RadioButton();
+            nodeSmallVillage = new RadioButton();
+            nodeForestValley = new RadioButton();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            Travel = new TabPage();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.mmmmmmmmmmmm;
+            pictureBox1.Location = new Point(457, 8);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(641, 618);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // nodeRiverVillage
+            // 
+            nodeRiverVillage.AutoSize = true;
+            nodeRiverVillage.BackColor = Color.Transparent;
+            nodeRiverVillage.Location = new Point(803, 311);
+            nodeRiverVillage.Name = "nodeRiverVillage";
+            nodeRiverVillage.Size = new Size(14, 13);
+            nodeRiverVillage.TabIndex = 1;
+            nodeRiverVillage.TabStop = true;
+            nodeRiverVillage.UseVisualStyleBackColor = false;
+            // 
+            // nodeMounttown
+            // 
+            nodeMounttown.AutoSize = true;
+            nodeMounttown.BackColor = Color.Transparent;
+            nodeMounttown.Location = new Point(641, 266);
+            nodeMounttown.Name = "nodeMounttown";
+            nodeMounttown.Size = new Size(14, 13);
+            nodeMounttown.TabIndex = 2;
+            nodeMounttown.TabStop = true;
+            nodeMounttown.UseVisualStyleBackColor = false;
+            // 
+            // nodeDesert
+            // 
+            nodeDesert.AutoSize = true;
+            nodeDesert.BackColor = Color.Transparent;
+            nodeDesert.Location = new Point(963, 290);
+            nodeDesert.Name = "nodeDesert";
+            nodeDesert.Size = new Size(14, 13);
+            nodeDesert.TabIndex = 3;
+            nodeDesert.TabStop = true;
+            nodeDesert.UseVisualStyleBackColor = false;
+            // 
+            // nodeDarkSpire
+            // 
+            nodeDarkSpire.AutoSize = true;
+            nodeDarkSpire.BackColor = Color.Transparent;
+            nodeDarkSpire.Location = new Point(698, 221);
+            nodeDarkSpire.Name = "nodeDarkSpire";
+            nodeDarkSpire.Size = new Size(14, 13);
+            nodeDarkSpire.TabIndex = 4;
+            nodeDarkSpire.TabStop = true;
+            nodeDarkSpire.UseVisualStyleBackColor = false;
+            // 
+            // nodeSouthernIsland
+            // 
+            nodeSouthernIsland.AutoSize = true;
+            nodeSouthernIsland.BackColor = Color.Transparent;
+            nodeSouthernIsland.Location = new Point(663, 576);
+            nodeSouthernIsland.Name = "nodeSouthernIsland";
+            nodeSouthernIsland.Size = new Size(14, 13);
+            nodeSouthernIsland.TabIndex = 5;
+            nodeSouthernIsland.TabStop = true;
+            nodeSouthernIsland.UseVisualStyleBackColor = false;
+            // 
+            // nodeMountain
+            // 
+            nodeMountain.AutoSize = true;
+            nodeMountain.BackColor = Color.Transparent;
+            nodeMountain.Location = new Point(572, 164);
+            nodeMountain.Name = "nodeMountain";
+            nodeMountain.Size = new Size(14, 13);
+            nodeMountain.TabIndex = 6;
+            nodeMountain.TabStop = true;
+            nodeMountain.UseVisualStyleBackColor = false;
+            // 
+            // nodeForestPlains
+            // 
+            nodeForestPlains.AutoSize = true;
+            nodeForestPlains.BackColor = Color.Transparent;
+            nodeForestPlains.Location = new Point(963, 178);
+            nodeForestPlains.Name = "nodeForestPlains";
+            nodeForestPlains.Size = new Size(14, 13);
+            nodeForestPlains.TabIndex = 7;
+            nodeForestPlains.TabStop = true;
+            nodeForestPlains.UseVisualStyleBackColor = false;
+            // 
+            // nodeNorthernIsland
+            // 
+            nodeNorthernIsland.AutoSize = true;
+            nodeNorthernIsland.BackColor = Color.Transparent;
+            nodeNorthernIsland.Location = new Point(781, 93);
+            nodeNorthernIsland.Name = "nodeNorthernIsland";
+            nodeNorthernIsland.Size = new Size(14, 13);
+            nodeNorthernIsland.TabIndex = 8;
+            nodeNorthernIsland.TabStop = true;
+            nodeNorthernIsland.UseVisualStyleBackColor = false;
+            // 
+            // nodeFarCliffs
+            // 
+            nodeFarCliffs.AutoSize = true;
+            nodeFarCliffs.BackColor = Color.Transparent;
+            nodeFarCliffs.Location = new Point(1057, 178);
+            nodeFarCliffs.Name = "nodeFarCliffs";
+            nodeFarCliffs.Size = new Size(14, 13);
+            nodeFarCliffs.TabIndex = 9;
+            nodeFarCliffs.TabStop = true;
+            nodeFarCliffs.UseVisualStyleBackColor = false;
+            // 
+            // nodeSmallVillage
+            // 
+            nodeSmallVillage.AutoSize = true;
+            nodeSmallVillage.BackColor = Color.Transparent;
+            nodeSmallVillage.Location = new Point(803, 385);
+            nodeSmallVillage.Name = "nodeSmallVillage";
+            nodeSmallVillage.Size = new Size(14, 13);
+            nodeSmallVillage.TabIndex = 10;
+            nodeSmallVillage.TabStop = true;
+            nodeSmallVillage.UseVisualStyleBackColor = false;
+            // 
+            // nodeForestValley
+            // 
+            nodeForestValley.AutoSize = true;
+            nodeForestValley.BackColor = Color.Transparent;
+            nodeForestValley.Location = new Point(832, 193);
+            nodeForestValley.Name = "nodeForestValley";
+            nodeForestValley.Size = new Size(14, 13);
+            nodeForestValley.TabIndex = 11;
+            nodeForestValley.TabStop = true;
+            nodeForestValley.UseVisualStyleBackColor = false;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(Travel);
+            tabControl1.Location = new Point(12, 13);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(439, 617);
+            tabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(431, 589);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Activities";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Travel
+            // 
+            Travel.Location = new Point(4, 24);
+            Travel.Name = "Travel";
+            Travel.Padding = new Padding(3);
+            Travel.Size = new Size(431, 589);
+            Travel.TabIndex = 1;
+            Travel.Text = "Travel";
+            Travel.UseVisualStyleBackColor = true;
+            // 
+            // NavigationWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1110, 640);
+            Controls.Add(tabControl1);
+            Controls.Add(nodeForestValley);
+            Controls.Add(nodeSmallVillage);
+            Controls.Add(nodeFarCliffs);
+            Controls.Add(nodeNorthernIsland);
+            Controls.Add(nodeForestPlains);
+            Controls.Add(nodeMountain);
+            Controls.Add(nodeSouthernIsland);
+            Controls.Add(nodeDarkSpire);
+            Controls.Add(nodeDesert);
+            Controls.Add(nodeMounttown);
+            Controls.Add(nodeRiverVillage);
+            Controls.Add(pictureBox1);
+            Name = "NavigationWindow";
+            Text = "NavigationWindow";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private RadioButton nodeRiverVillage;
+        private RadioButton nodeMounttown;
+        private RadioButton nodeDesert;
+        private RadioButton nodeDarkSpire;
+        private RadioButton nodeSouthernIsland;
+        private RadioButton nodeMountain;
+        private RadioButton nodeForestPlains;
+        private RadioButton nodeNorthernIsland;
+        private RadioButton nodeFarCliffs;
+        private RadioButton nodeSmallVillage;
+        private RadioButton nodeForestValley;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage Travel;
     }
 }
