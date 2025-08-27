bfe4e9701b5ab8a11ec69d46f20593cf4ace939b
bullshag <78245141+bullshag@users.noreply.github.com>
2025-08-26 19:52:49 -0500

    l

--- WinFormsApp2/NavigationWindow.Designer.cs
+++ WinFormsApp2/NavigationWindow.Designer.cs
@@ -43,25 +43,28 @@
             nodeForestValley = new RadioButton();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
-            Travel = new TabPage();
-            btnShop = new Button();
-            btnGraveyard = new Button();
-            btnTavern = new Button();
-            btnFindEnemies = new Button();
-            btnArena = new Button();
             btnTemple = new Button();
-            lstConnections = new ListBox();
-            rtbNodeDescription = new RichTextBox();
-            btnBeginTravel = new Button();
-            travelProgressBar = new ProgressBar();
+            btnArena = new Button();
+            btnFindEnemies = new Button();
+            btnTavern = new Button();
+            btnGraveyard = new Button();
+            btnShop = new Button();
+            Travel = new TabPage();
             lblTravelInfo = new Label();
+            travelProgressBar = new ProgressBar();
+            btnBeginTravel = new Button();
+            rtbNodeDescription = new RichTextBox();
+            lstConnections = new ListBox();
             toolTip1 = new ToolTip(components);
+            locationLabel = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            Travel.SuspendLayout();
             SuspendLayout();
-            //
+            // 
             // pictureBox1
-            //
+            // 
             pictureBox1.Image = Properties.Resources.mmmmmmmmmmmm;
             pictureBox1.Location = new Point(457, 8);
             pictureBox1.Name = "pictureBox1";
@@ -71,159 +74,159 @@
             // 
             // nodeRiverVillage
             // 
+            nodeRiverVillage.AutoCheck = false;
             nodeRiverVillage.AutoSize = true;
             nodeRiverVillage.BackColor = Color.Transparent;
+            nodeRiverVillage.Enabled = false;
             nodeRiverVillage.Location = new Point(803, 311);
             nodeRiverVillage.Name = "nodeRiverVillage";
             nodeRiverVillage.Size = new Size(14, 13);
             nodeRiverVillage.TabIndex = 1;
             nodeRiverVillage.TabStop = true;
-            nodeRiverVillage.AutoCheck = false;
-            nodeRiverVillage.Enabled = false;
             nodeRiverVillage.UseVisualStyleBackColor = false;
             // 
             // nodeMounttown
             // 
+            nodeMounttown.AutoCheck = false;
             nodeMounttown.AutoSize = true;
             nodeMounttown.BackColor = Color.Transparent;
+            nodeMounttown.Enabled = false;
             nodeMounttown.Location = new Point(641, 266);
             nodeMounttown.Name = "nodeMounttown";
             nodeMounttown.Size = new Size(14, 13);
             nodeMounttown.TabIndex = 2;
             nodeMounttown.TabStop = true;
-            nodeMounttown.AutoCheck = false;
-            nodeMounttown.Enabled = false;
             nodeMounttown.UseVisualStyleBackColor = false;
             // 
             // nodeDesert
             // 
+            nodeDesert.AutoCheck = false;
             nodeDesert.AutoSize = true;
             nodeDesert.BackColor = Color.Transparent;
+            nodeDesert.Enabled = false;
             nodeDesert.Location = new Point(963, 290);
             nodeDesert.Name = "nodeDesert";
             nodeDesert.Size = new Size(14, 13);
             nodeDesert.TabIndex = 3;
             nodeDesert.TabStop = true;
-            nodeDesert.AutoCheck = false;
-            nodeDesert.Enabled = false;
             nodeDesert.UseVisualStyleBackColor = false;
             // 
             // nodeDarkSpire
             // 
+            nodeDarkSpire.AutoCheck = false;
             nodeDarkSpire.AutoSize = true;
             nodeDarkSpire.BackColor = Color.Transparent;
+            nodeDarkSpire.Enabled = false;
             nodeDarkSpire.Location = new Point(698, 221);
             nodeDarkSpire.Name = "nodeDarkSpire";
             nodeDarkSpire.Size = new Size(14, 13);
             nodeDarkSpire.TabIndex = 4;
             nodeDarkSpire.TabStop = true;
-            nodeDarkSpire.AutoCheck = false;
-            nodeDarkSpire.Enabled = false;
             nodeDarkSpire.UseVisualStyleBackColor = false;
             // 
             // nodeSouthernIsland
             // 
+            nodeSouthernIsland.AutoCheck = false;
             nodeSouthernIsland.AutoSize = true;
             nodeSouthernIsland.BackColor = Color.Transparent;
+            nodeSouthernIsland.Enabled = false;
             nodeSouthernIsland.Location = new Point(663, 576);
             nodeSouthernIsland.Name = "nodeSouthernIsland";
             nodeSouthernIsland.Size = new Size(14, 13);
             nodeSouthernIsland.TabIndex = 5;
             nodeSouthernIsland.TabStop = true;
-            nodeSouthernIsland.AutoCheck = false;
-            nodeSouthernIsland.Enabled = false;
             nodeSouthernIsland.UseVisualStyleBackColor = false;
             // 
             // nodeMountain
             // 
+            nodeMountain.AutoCheck = false;
             nodeMountain.AutoSize = true;
             nodeMountain.BackColor = Color.Transparent;
+            nodeMountain.Enabled = false;
             nodeMountain.Location = new Point(572, 164);
             nodeMountain.Name = "nodeMountain";
             nodeMountain.Size = new Size(14, 13);
             nodeMountain.TabIndex = 6;
             nodeMountain.TabStop = true;
-            nodeMountain.AutoCheck = false;
-            nodeMountain.Enabled = false;
             nodeMountain.UseVisualStyleBackColor = false;
             // 
             // nodeForestPlains
             // 
+            nodeForestPlains.AutoCheck = false;
             nodeForestPlains.AutoSize = true;
             nodeForestPlains.BackColor = Color.Transparent;
+            nodeForestPlains.Enabled = false;
             nodeForestPlains.Location = new Point(963, 178);
             nodeForestPlains.Name = "nodeForestPlains";
             nodeForestPlains.Size = new Size(14, 13);
             nodeForestPlains.TabIndex = 7;
             nodeForestPlains.TabStop = true;
-            nodeForestPlains.AutoCheck = false;
-            nodeForestPlains.Enabled = false;
             nodeForestPlains.UseVisualStyleBackColor = false;
             // 
             // nodeNorthernIsland
             // 
+            nodeNorthernIsland.AutoCheck = false;
             nodeNorthernIsland.AutoSize = true;
             nodeNorthernIsland.BackColor = Color.Transparent;
+            nodeNorthernIsland.Enabled = false;
             nodeNorthernIsland.Location = new Point(781, 93);
             nodeNorthernIsland.Name = "nodeNorthernIsland";
             nodeNorthernIsland.Size = new Size(14, 13);
             nodeNorthernIsland.TabIndex = 8;
             nodeNorthernIsland.TabStop = true;
-            nodeNorthernIsland.AutoCheck = false;
-            nodeNorthernIsland.Enabled = false;
             nodeNorthernIsland.UseVisualStyleBackColor = false;
             // 
             // nodeFarCliffs
             // 
+            nodeFarCliffs.AutoCheck = false;
             nodeFarCliffs.AutoSize = true;
             nodeFarCliffs.BackColor = Color.Transparent;
+            nodeFarCliffs.Enabled = false;
             nodeFarCliffs.Location = new Point(1057, 178);
             nodeFarCliffs.Name = "nodeFarCliffs";
             nodeFarCliffs.Size = new Size(14, 13);
             nodeFarCliffs.TabIndex = 9;
             nodeFarCliffs.TabStop = true;
-            nodeFarCliffs.AutoCheck = false;
-            nodeFarCliffs.Enabled = false;
             nodeFarCliffs.UseVisualStyleBackColor = false;
             // 
             // nodeSmallVillage
             // 
+            nodeSmallVillage.AutoCheck = false;
             nodeSmallVillage.AutoSize = true;
             nodeSmallVillage.BackColor = Color.Transparent;
+            nodeSmallVillage.Enabled = false;
             nodeSmallVillage.Location = new Point(803, 385);
             nodeSmallVillage.Name = "nodeSmallVillage";
             nodeSmallVillage.Size = new Size(14, 13);
             nodeSmallVillage.TabIndex = 10;
             nodeSmallVillage.TabStop = true;
-            nodeSmallVillage.AutoCheck = false;
-            nodeSmallVillage.Enabled = false;
             nodeSmallVillage.UseVisualStyleBackColor = false;
             // 
             // nodeForestValley
             // 
+            nodeForestValley.AutoCheck = false;
             nodeForestValley.AutoSize = true;
             nodeForestValley.BackColor = Color.Transparent;
+            nodeForestValley.Enabled = false;
             nodeForestValley.Location = new Point(832, 193);
             nodeForestValley.Name = "nodeForestValley";
             nodeForestValley.Size = new Size(14, 13);
             nodeForestValley.TabIndex = 11;
             nodeForestValley.TabStop = true;
-            nodeForestValley.AutoCheck = false;
-            nodeForestValley.Enabled = false;
             nodeForestValley.UseVisualStyleBackColor = false;
             // 
             // tabControl1
             // 
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(Travel);
-            tabControl1.Location = new Point(12, 13);
+            tabControl1.Location = new Point(12, 36);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(439, 617);
+            tabControl1.Size = new Size(439, 594);
             tabControl1.TabIndex = 12;
             // 
             // tabPage1
-            //
+            // 
             tabPage1.Controls.Add(btnTemple);
             tabPage1.Controls.Add(btnArena);
             tabPage1.Controls.Add(btnFindEnemies);
@@ -233,58 +236,13 @@
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(431, 589);
+            tabPage1.Size = new Size(431, 566);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Activities";
             tabPage1.UseVisualStyleBackColor = true;
-            //
-            // btnShop
-            //
-            btnShop.Location = new Point(6, 6);
-            btnShop.Name = "btnShop";
-            btnShop.Size = new Size(150, 23);
-            btnShop.TabIndex = 0;
-            btnShop.Text = "Shop";
-            btnShop.UseVisualStyleBackColor = true;
-            //
-            // btnGraveyard
-            //
-            btnGraveyard.Location = new Point(6, 35);
-            btnGraveyard.Name = "btnGraveyard";
-            btnGraveyard.Size = new Size(150, 23);
-            btnGraveyard.TabIndex = 1;
-            btnGraveyard.Text = "Graveyard";
-            btnGraveyard.UseVisualStyleBackColor = true;
-            //
-            // btnTavern
-            //
-            btnTavern.Location = new Point(6, 64);
-            btnTavern.Name = "btnTavern";
-            btnTavern.Size = new Size(150, 23);
-            btnTavern.TabIndex = 2;
-            btnTavern.Text = "Tavern";
-            btnTavern.UseVisualStyleBackColor = true;
-            //
-            // btnFindEnemies
-            //
-            btnFindEnemies.Location = new Point(6, 93);
-            btnFindEnemies.Name = "btnFindEnemies";
-            btnFindEnemies.Size = new Size(150, 23);
-            btnFindEnemies.TabIndex = 3;
-            btnFindEnemies.Text = "Find Enemies";
-            btnFindEnemies.UseVisualStyleBackColor = true;
-            //
-            // btnArena
-            //
-            btnArena.Location = new Point(6, 122);
-            btnArena.Name = "btnArena";
-            btnArena.Size = new Size(150, 23);
-            btnArena.TabIndex = 4;
-            btnArena.Text = "Battle Arena";
-            btnArena.UseVisualStyleBackColor = true;
-            //
+            // 
             // btnTemple
-            //
+            // 
             btnTemple.Location = new Point(6, 151);
             btnTemple.Name = "btnTemple";
             btnTemple.Size = new Size(150, 23);
@@ -292,8 +250,53 @@
             btnTemple.Text = "Temple";
             btnTemple.UseVisualStyleBackColor = true;
             // 
+            // btnArena
+            // 
+            btnArena.Location = new Point(6, 122);
+            btnArena.Name = "btnArena";
+            btnArena.Size = new Size(150, 23);
+            btnArena.TabIndex = 4;
+            btnArena.Text = "Battle Arena";
+            btnArena.UseVisualStyleBackColor = true;
+            // 
+            // btnFindEnemies
+            // 
+            btnFindEnemies.Location = new Point(6, 93);
+            btnFindEnemies.Name = "btnFindEnemies";
+            btnFindEnemies.Size = new Size(150, 23);
+            btnFindEnemies.TabIndex = 3;
+            btnFindEnemies.Text = "Find Enemies";
+            btnFindEnemies.UseVisualStyleBackColor = true;
+            // 
+            // btnTavern
+            // 
+            btnTavern.Location = new Point(6, 64);
+            btnTavern.Name = "btnTavern";
+            btnTavern.Size = new Size(150, 23);
+            btnTavern.TabIndex = 2;
+            btnTavern.Text = "Tavern";
+            btnTavern.UseVisualStyleBackColor = true;
+            // 
+            // btnGraveyard
+            // 
+            btnGraveyard.Location = new Point(6, 35);
+            btnGraveyard.Name = "btnGraveyard";
+            btnGraveyard.Size = new Size(150, 23);
+            btnGraveyard.TabIndex = 1;
+            btnGraveyard.Text = "Graveyard";
+            btnGraveyard.UseVisualStyleBackColor = true;
+            // 
+            // btnShop
+            // 
+            btnShop.Location = new Point(6, 6);
+            btnShop.Name = "btnShop";
+            btnShop.Size = new Size(150, 23);
+            btnShop.TabIndex = 0;
+            btnShop.Text = "Shop";
+            btnShop.UseVisualStyleBackColor = true;
+            // 
             // Travel
-            //
+            // 
             Travel.Controls.Add(lblTravelInfo);
             Travel.Controls.Add(travelProgressBar);
             Travel.Controls.Add(btnBeginTravel);
@@ -306,18 +309,24 @@
             Travel.TabIndex = 1;
             Travel.Text = "Travel";
             Travel.UseVisualStyleBackColor = true;
-            //
-            // lstConnections
-            //
-            lstConnections.FormattingEnabled = true;
-            lstConnections.ItemHeight = 15;
-            lstConnections.Location = new Point(6, 6);
-            lstConnections.Name = "lstConnections";
-            lstConnections.Size = new Size(417, 199);
-            lstConnections.TabIndex = 0;
-            //
+            // 
+            // lblTravelInfo
+            // 
+            lblTravelInfo.AutoSize = true;
+            lblTravelInfo.Location = new Point(9, 355);
+            lblTravelInfo.Name = "lblTravelInfo";
+            lblTravelInfo.Size = new Size(0, 15);
+            lblTravelInfo.TabIndex = 4;
+            // 
+            // travelProgressBar
+            // 
+            travelProgressBar.Location = new Point(6, 326);
+            travelProgressBar.Name = "travelProgressBar";
+            travelProgressBar.Size = new Size(417, 23);
+            travelProgressBar.TabIndex = 3;
+            // 
             // btnBeginTravel
-            //
+            // 
             btnBeginTravel.Location = new Point(6, 297);
             btnBeginTravel.Name = "btnBeginTravel";
             btnBeginTravel.Size = new Size(150, 23);
@@ -325,25 +334,9 @@
             btnBeginTravel.Text = "Begin Travel";
             btnBeginTravel.UseVisualStyleBackColor = true;
             btnBeginTravel.Click += btnBeginTravel_Click;
-            //
-            // travelProgressBar
-            //
-            travelProgressBar.Location = new Point(6, 326);
-            travelProgressBar.Name = "travelProgressBar";
-            travelProgressBar.Size = new Size(417, 23);
-            travelProgressBar.TabIndex = 3;
-            //
-            // lblTravelInfo
-            //
-            lblTravelInfo.AutoSize = true;
-            lblTravelInfo.Location = new Point(6, 352);
-            lblTravelInfo.Name = "lblTravelInfo";
-            lblTravelInfo.Size = new Size(0, 15);
-            lblTravelInfo.TabIndex = 4;
-
-            //
+            // 
             // rtbNodeDescription
-            //
+            // 
             rtbNodeDescription.Location = new Point(6, 211);
             rtbNodeDescription.Name = "rtbNodeDescription";
             rtbNodeDescription.ReadOnly = true;
@@ -351,11 +344,31 @@
             rtbNodeDescription.TabIndex = 1;
             rtbNodeDescription.Text = "";
             // 
+            // lstConnections
+            // 
+            lstConnections.FormattingEnabled = true;
+            lstConnections.ItemHeight = 15;
+            lstConnections.Location = new Point(6, 6);
+            lstConnections.Name = "lstConnections";
+            lstConnections.Size = new Size(417, 199);
+            lstConnections.TabIndex = 0;
+            // 
+            // locationLabel
+            // 
+            locationLabel.AutoSize = true;
+            locationLabel.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            locationLabel.Location = new Point(97, 14);
+            locationLabel.Name = "locationLabel";
+            locationLabel.Size = new Size(211, 19);
+            locationLabel.TabIndex = 13;
+            locationLabel.Text = "Current Location: XXXXX";
+            // 
             // NavigationWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1110, 640);
+            Controls.Add(locationLabel);
             Controls.Add(tabControl1);
             Controls.Add(nodeForestValley);
             Controls.Add(nodeSmallVillage);
@@ -373,6 +386,9 @@
             Text = "NavigationWindow";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            Travel.ResumeLayout(false);
+            Travel.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -406,5 +422,6 @@
         private Label lblTravelInfo;
         private RichTextBox rtbNodeDescription;
         private ToolTip toolTip1;
+        private Label locationLabel;
     }
 }
